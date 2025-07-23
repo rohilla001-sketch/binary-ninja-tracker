7412171e8148ca725d2666091c0c1aa1716b13cf
rohilla001-sketch <rohillahimanshu167@gmail.com>
2025-07-23 23:01:53 +0530

    Create App.tsx

---
+++ client/client/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Binary Ninja Tracker</h1>
+      <p>This is the frontend running on Vite + React!</p>
+    </div>
+  );
+}
+
+export default App;
